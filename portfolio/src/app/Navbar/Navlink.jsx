15f86b37d563b2c3da1c30c6d207a5bc9ccef26c
Jeeva Nivasan Ramasamy <jeevanivasanramasamy@gmail.com>
2023-12-05 17:16:58 +0530

    sixth commit

--- portfolio/src/app/Navbar/Navlink.jsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-const NavLink = ({ href, title }) => {
-    return (
-        <Link href={href} className='block py-3 pl-3 pr-4 text-[#ffffff] sm:text-xl rounded md:p-0 hover:text-orange-600'>{title}</Link>
-    );
-};
-
-export default NavLink;
